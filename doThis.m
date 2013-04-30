6af9a23a74fefb4bcf77c7908fa4d69e3cb55ee7
Matthew Farmer <87nolaguy@gmail.com>
2013-04-29 19:59:46 -0500

    more info added about fundamental matrix

--- doThis.m
+++ doThis.m
@@ -27,6 +27,18 @@ R = focalLength1/focalLength2;
 [R1,rowMax1,colMax1] = harrisCorners(pathToImg1, 6, somethingHere, 10);
 [R2,rowMax2,colMax2] = harrisCorners(pathToImg2, 6, somethingHere, 10);
 
+%calculating the fundamental matrix
+%http://www-misa.cs.ucl.ac.uk/staff/S.Prince/4C75/hartley.pdf 8 point
+%example matlab code
+%http://feelmare.blogspot.com/2011/11/8-point-algorithm-matlab-source-code.html
+%more example matlab code
+%http://www.csse.uwa.edu.au/~pk/Research/MatlabFns/Projective/fundmatrix.m
+%good slide set on 8 point fundamental matrix computation
+%http://ece631web.groups.et.byu.net/Lectures/ECEn631%2013%20-%208%20Point%20Algorithm.pdf
+%if the 8 point isnt giving good results, use this paper to take the 8point
+%and make it better
+%http://research.microsoft.com/en-us/um/people/cloop/ZhangLoopCVIU-01.pdf
+
 %calculate homography matrix using using homography_solve func
 %http://www.mathworks.com/matlabcentral/answers/26141
 
